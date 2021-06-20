38022ebf27efe863682d2268c54d6b286a98beb9
Julius <espazo@stu.jsu.edu.cn>
2021-06-21 01:36:58 +0800

    2-8 ADD custom configuration

--- src/main/java/com/espazo/wiki/controller/TestController.java
+++ src/main/java/com/espazo/wiki/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.espazo.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
     /**
      * GET, POST, PUT, DELETE
@@ -20,7 +24,7 @@ public class TestController {
 //    @RequestMapping(vlaue="/user/1", method = RequestMethod.DELETE)
     @GetMapping("/hello")
     public String hello() {
-        return "hello interface GET";
+        return "hello interface GET " + testHello;
     }
 
     @PostMapping("/hello/post")
