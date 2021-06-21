19a32ba7f39a0dc04e862e92890b369fce9f4f63
Julius <espazo@stu.jsu.edu.cn>
2021-06-21 14:30:32 +0800

    3-1 ADD script all.sql

---
+++ doc/all.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `name` VARCHAR(50) COMMENT 'name',
+    `password` VARCHAR(50) COMMENT 'password',
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COMMENT='test'
