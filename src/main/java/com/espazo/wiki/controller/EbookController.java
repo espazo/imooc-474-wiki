c443dca7899cf77e2b1f765abea4550c81dfa83e
Julius <espazo@stu.jsu.edu.cn>
2021-06-22 14:49:45 +0800

    3-7 ADD encapsulation parameter for request and response

--- src/main/java/com/espazo/wiki/controller/EbookController.java
+++ src/main/java/com/espazo/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.espazo.wiki.controller;
 
-import com.espazo.wiki.domain.Ebook;
+import com.espazo.wiki.req.EbookReq;
 import com.espazo.wiki.resp.CommonResp;
+import com.espazo.wiki.resp.EbookResp;
 import com.espazo.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
