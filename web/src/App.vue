f3aea533de6813e069c4341487386d01366c80d3
Julius <espazo@stu.jsu.edu.cn>
2021-06-22 18:13:14 +0800

    4-7 ADD components `the-footer`

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,21 +2,20 @@
   <a-layout>
     <the-header/>
     <router-view/>
-
-    <a-layout-footer style="text-align: center">
-      Rui Xiao - WIKI BOOK
-    </a-layout-footer>
+    <the-footer/>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 });
 </script>
