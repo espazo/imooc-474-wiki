95f5a33d96d3d4042e4f2780e0e02a5ea79a57a0
Julius <espazo@stu.jsu.edu.cn>
2021-06-22 17:00:50 +0800

    4-5 MODIFY integrated `Ant Design Vue`, and add `button` example

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <a-button type="danger">Danger</a-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
