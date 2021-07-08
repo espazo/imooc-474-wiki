c46e87d7f62e8b200d1008f1a109fcfbe4b4c82d
Julius <espazo@stu.jsu.edu.cn>
2021-07-09 00:44:37 +0800

    7-2 分类表设计与持久层代码生成 基本的无限级树形表结构设计：id + parent

---
+++ src/main/java/com/espazo/wiki/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.espazo.wiki.mapper;
+
+import com.espazo.wiki.domain.Category;
+import com.espazo.wiki.domain.CategoryExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+}
