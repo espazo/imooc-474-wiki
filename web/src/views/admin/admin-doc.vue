3c46e9217106701c0e55a527b6d8e43e04a475a8
Julius <espazo@stu.jsu.edu.cn>
2021-07-09 23:21:25 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -20,6 +20,7 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :data-source="level1"
               :loading="loading"
@@ -27,6 +28,7 @@
               :row-key="record => record.id"
               size="small"
               @change="handleTableChange"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{ record.sort }} {{ text }}
@@ -163,7 +165,7 @@ export default defineComponent({
      * }]
      **/
     const level1 = ref();
-
+    level1.value = [];
     /**
      * 数据查询
      **/
