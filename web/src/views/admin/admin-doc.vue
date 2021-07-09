be0574e8d565ecd0c68cf693e7b613c4251d2b67
Julius <espazo@stu.jsu.edu.cn>
2021-07-10 01:46:22 +0800

    8-11 增加读取文档内容功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -280,7 +280,22 @@ export default defineComponent({
           }
         }
       }
-    }
+    };
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = () => {
+      axios.get("/doc/find-content/" + doc.value.id).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          editor.txt.html(data.content);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
 
     /**
      * 编辑
@@ -288,6 +303,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
+      handleQueryContent();
 
       // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
       treeSelectData.value = Tool.copy(level1.value);
