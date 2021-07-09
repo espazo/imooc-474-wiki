64d9616a23052d47341c71c290e67aa9bd7257dc
Julius <espazo@stu.jsu.edu.cn>
2021-07-09 20:33:52 +0800

    8-6 删除某个文档时，增加二次确认

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -37,7 +37,7 @@
                 cancel-text="否 "
                 ok-text="是"
                 title="删除后不可恢复，确认删除？"
-                @confirm="handleDelete(record.id)"
+                @confirm="showConfirm(record.id)"
             >
               <a-button type="danger">
                 删除
@@ -95,7 +95,7 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
-import {message} from 'ant-design-vue'
+import {message, Modal} from 'ant-design-vue'
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
 
@@ -228,6 +228,7 @@ export default defineComponent({
     }
 
     const ids: Array<string> = [];
+    const names: Array<string> = [];
     /**
      * 查找整根树枝
      */
@@ -242,6 +243,7 @@ export default defineComponent({
           // 将目标 id 放入结果集 ids 中
           // node.disabled = true;
           ids.push(id);
+          names.push(node.name);
 
           // 遍历所有子节点
           const children = node.children;
@@ -294,14 +296,32 @@ export default defineComponent({
      * 删除
      */
     const handleDelete = (id: number) => {
-      ids.length = 0;
-      console.log('ids', ids);
-      getDeleteIds(level1.value, id);
+      // ids.length = 0;
+      // console.log('ids', ids);
+      // getDeleteIds(level1.value, id);
       axios.delete("/doc/delete/" + ids.join(',')).then((response) => {
         const data = response.data;
         if (data.success) {
           handleQuery();
         }
+      });
+    };
+
+    const showConfirm = (id: number) => {
+      ids.length = 0;
+      names.length = 0;
+      getDeleteIds(level1.value, id);
+
+      Modal.confirm({
+        title: '重要提醒',
+        // icon: ExclamationCircleOutlined,
+        content: '将删除：【' + names.join('，') + '】，删除后不可恢复。确认删除？',
+        onOk() {
+          handleDelete(id);
+        },
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onCancel() {
+        },
       });
     }
 
@@ -328,7 +348,8 @@ export default defineComponent({
       modalLoading,
       handleModalOK,
 
-      handleQuery
+      handleQuery,
+      showConfirm,
     }
   }
 });
